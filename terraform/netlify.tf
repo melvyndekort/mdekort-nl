324a9b78ca1f2afa69ae8893c4885451736e8cfd
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 16:49:51 +0200

    Use correct branch

--- terraform/netlify.tf
+++ terraform/netlify.tf
@@ -2,7 +2,7 @@ resource "netlify_site" "mdekort-nl" {
   name = "mdekort-nl"
 
   repo {
-    repo_branch = "master"
+    repo_branch = "main"
     dir         = "src"
     provider    = "github"
     repo_path   = "melvyndekort/mdekort-nl"
