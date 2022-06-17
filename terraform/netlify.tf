2692f9d29d9ece01f8b07aaec4359511a537c816
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 16:46:36 +0200

    Manage the netlify site here as well

---
+++ terraform/netlify.tf
@@ -0,0 +1,10 @@
+resource "netlify_site" "mdekort-nl" {
+  name = "mdekort-nl"
+
+  repo {
+    repo_branch   = "master"
+    dir           = "src"
+    provider      = "github"
+    repo_path     = "melvyndekort/mdekort-nl"
+  }
+}
