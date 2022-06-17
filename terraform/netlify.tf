b99202db6195b6e683eabf1cc7209f935f125de2
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 16:47:24 +0200

    Terraform format

--- terraform/netlify.tf
+++ terraform/netlify.tf
@@ -2,9 +2,9 @@ resource "netlify_site" "mdekort-nl" {
   name = "mdekort-nl"
 
   repo {
-    repo_branch   = "master"
-    dir           = "src"
-    provider      = "github"
-    repo_path     = "melvyndekort/mdekort-nl"
+    repo_branch = "master"
+    dir         = "src"
+    provider    = "github"
+    repo_path   = "melvyndekort/mdekort-nl"
   }
 }
