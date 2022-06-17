18facc0621e788d5acd339553495b33afa241935
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-17 15:57:03 +0200

    No need to assume a role

--- terraform/state.tf
+++ terraform/state.tf
@@ -3,7 +3,6 @@ terraform {
     bucket     = "mdekort.tfstate"
     key        = "apex-mdekort-nl.tfstate"
     region     = "eu-west-1"
-    role_arn   = "arn:aws:iam::075673041815:role/AdminRole"
     encrypt    = "true"
     kms_key_id = "arn:aws:kms:eu-west-1:075673041815:alias/generic"
   }
