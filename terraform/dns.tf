9917951fea7ca7ce983765fa3ea0781f122e4293
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-18 03:13:20 +0200

    Beautify terraform

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_record" "apex" {
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
-  name    = ""
+  name    = "@"
   type    = "CNAME"
-  ttl     = "300"
+  ttl     = 300
   value   = "apex-loadbalancer.netlify.com"
 }
 
@@ -10,6 +10,6 @@ resource "cloudflare_record" "www" {
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
   name    = "www"
   type    = "CNAME"
-  ttl     = "300"
+  ttl     = 300
   value   = "apex-mdekort-nl.netlify.app"
 }
