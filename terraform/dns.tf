7c3c6428fc68a7219e469ee28a9304b1c555262b
Melvyn de Kort <melvyn@mdekort.nl>
2022-07-05 11:32:53 +0200

    More generic Terraform setup

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,17 +1,19 @@
 resource "cloudflare_record" "apex" {
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
-  name    = "@"
+  name    = var.domain_name
   type    = "CNAME"
   ttl     = 1
   proxied = false
   value   = "apex-loadbalancer.netlify.com"
 }
 
-resource "cloudflare_record" "www" {
+resource "cloudflare_record" "aliasses" {
+  for_each = toset(var.domain_aliasses)
+
   zone_id = data.terraform_remote_state.cloudsetup.outputs.mdekort_zone_id
-  name    = "www"
+  name    = each.key
   type    = "CNAME"
   ttl     = 1
   proxied = false
-  value   = "apex-mdekort-nl.netlify.app"
+  value   = "${var.name}.netlify.app"
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+name            = "mdekort-nl"
+repo            = "melvyndekort/mdekort-nl"
+domain_name     = "mdekort.nl"
+domain_aliasses = ["www.mdekort.nl"]
