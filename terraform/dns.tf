be826a11661447636d3e57941ac7654a352af87a
Melvyn de Kort <melvyn@mdekort.nl>
2022-12-24 17:32:09 +0100

    Migrate from Netlify to Cloudflare Pages

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,9 +4,18 @@ data "cloudflare_zones" "zone" {
   }
 }
 
-resource "cloudflare_record" "site" {
+resource "cloudflare_record" "apex" {
   zone_id = data.cloudflare_zones.zone.zones[0].id
   name    = var.domain_name
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+  value   = "${var.name}.pages.dev"
+}
+
+resource "cloudflare_record" "site" {
+  zone_id = data.cloudflare_zones.zone.zones[0].id
+  name    = "www.${var.domain_name}"
   type    = "CNAME"
   ttl     = 1
   proxied = false
--- terraform/pages.tf
+++ terraform/pages.tf
@@ -4,10 +4,21 @@ resource "cloudflare_pages_project" "site" {
   production_branch = var.repo_branch
 }
 
+resource "cloudflare_pages_domain" "apex" {
+  account_id   = local.cloudflare_account_id
+  project_name = cloudflare_pages_project.site.name
+  domain       = cloudflare_record.apex.hostname
+
+  depends_on = [
+    cloudflare_pages_project.site,
+    cloudflare_record.apex,
+  ]
+}
+
 resource "cloudflare_pages_domain" "site" {
   account_id   = local.cloudflare_account_id
   project_name = cloudflare_pages_project.site.name
-  domain       = var.domain_name
+  domain       = cloudflare_record.site.hostname
 
   depends_on = [
     cloudflare_pages_project.site,
